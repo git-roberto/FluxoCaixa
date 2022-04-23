193fe72f8bdf15b787ccc62eedbe8a34a9118fca
RobertoOliveira <RobertoOliveira@IROBERTO>
2022-04-23 16:15:35 -0300

    Autorização nas controllers

--- FluxoCaixa.API/Controllers/LancamentoController.cs
+++ FluxoCaixa.API/Controllers/LancamentoController.cs
@@ -9,11 +9,13 @@ using System.Web.Mvc;
 using Utilidades.Controllers;
 using Utilidades.DTO;
 using Utilidades.Json;
+using Utilidades.JWT;
 
 namespace FluxoCaixa.API.Controllers
 {
     public class LancamentoController : BaseController
     {
+        [Autorizacao]
         public ActionResult Index()
         {
             using (LancamentoService service = new LancamentoService())
@@ -32,6 +34,7 @@ namespace FluxoCaixa.API.Controllers
             }
         }
 
+        [Autorizacao]
         public ActionResult Buscar(int id)
         {
             using (LancamentoService service = new LancamentoService())
@@ -48,6 +51,7 @@ namespace FluxoCaixa.API.Controllers
         }
 
         [HttpPost]
+        [Autorizacao]
         public ActionResult Inserir(LancamentoDTO registro)
         {
             using (LancamentoService service = new LancamentoService())
@@ -65,6 +69,7 @@ namespace FluxoCaixa.API.Controllers
         }
 
         [HttpPost]
+        [Autorizacao]
         public ActionResult Alterar(LancamentoDTO registro)
         {
             using (LancamentoService service = new LancamentoService())
@@ -82,6 +87,7 @@ namespace FluxoCaixa.API.Controllers
         }
 
         [HttpPost]
+        [Autorizacao]
         public ActionResult Excluir(int id)
         {
             using (LancamentoService service = new LancamentoService())
@@ -97,6 +103,8 @@ namespace FluxoCaixa.API.Controllers
             }
         }
 
+        [HttpPost]
+        [Autorizacao]
         public ActionResult ConsolidadoDiario()
         {
             using (LancamentoService service = new LancamentoService())
